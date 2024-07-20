4c26224f476318f139747be29e62e087d10d000d
Kei Duke Bergman <54135223+Brainthread@users.noreply.github.com>
2024-07-20 13:03:23 +0200

    Implement crude fade-in

--- scripts/TileManagement/BioTile.gd
+++ scripts/TileManagement/BioTile.gd
@@ -11,9 +11,20 @@ var grid_position:Vector2i
 var map_handler:BioTileMap
 var neighbours:Array
 
+@export var fadein = false
+var startsize
+
 func _ready():
+	startsize = scale
+	if fadein:
+		scale = Vector2(0.7, 0.7)
 	for subsystem in on_tick_subsystems:
 		subsystem.interface = self
+
+func _process(delta):
+	if scale.x < 1:
+		scale += Vector2(delta, delta)
+	
 
 func on_placed():
 	for subsystem in on_tick_subsystems:
